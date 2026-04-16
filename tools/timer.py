768a90c1444bee8f02c13e2446f25b521c59ff86
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-04-17 00:35:48 +0500

    Added structure of future tools

---
+++ tools/timer.py
@@ -0,0 +1,12 @@
+from time import perf_counter
+from foonctools import wraps
+
+class Time:
+    """
+    A tool for determinig the time takes to complete provided
+    function or task. Simple but timer but have many format
+    options to present time.
+    """
+
+    def __init__(self, format: OPtional) -> None:
+        self.time_format =
