cc723440d458c46c62e5556e86f10465187acf6c
Umidjon Khodjaev <253513646+Umidjon-Kh@users.noreply.github.com>
2026-05-12 16:47:54 +0500

    Heart beat base cls left it as is was too

--- vakt_refac/observer/core/ports/__init__.py
+++ vakt_refac/observer/core/ports/__init__.py
@@ -1,7 +1,9 @@
+from .heart_beat import BaseHeartBeat
 from .path_lock import BasePathLock
 from .watcher import BaseWatcher
 
 __all__ = [
     "BaseWatcher",
     "BasePathLock",
+    "BaseHeartBeat",
 ]
