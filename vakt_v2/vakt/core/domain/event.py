babf69c4ba452181b9fc0ab39a070ccad9b2f5b9
Umidjon Khodjaev <253513646+Umidjon-Kh@users.noreply.github.com>
2026-06-04 04:22:50 +0500

    rewrited instruction.py

--- vakt_v2/vakt/core/domain/event.py
+++ vakt_v2/vakt/core/domain/event.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Optional
 
-from .semantic_types import SemanticType
+from .semantic_type import SemanticType
 
 
 class EventType(SemanticType):
